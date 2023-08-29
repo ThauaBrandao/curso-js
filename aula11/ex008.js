f5bd52aed7d1186e3e23eab8d1e0be6c7612ef64
Thaua <brandaothaua@hotmail.com>
2023-08-29 11:44:52 -0300

    if feito

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+let vel = 60.5
+if (vel < 80) {
+    console.log(`A sua velocidade é de ${vel}Km/h, cuidado, o máximo é de 80Km/h !`)
+} else {
+    console.log('Você está acima da velocidade, MAX 80Km/h !')
+}
